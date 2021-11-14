fb96b1ea1a512f9af8f11e755f3a28e89e9346c9
ShovelDev <92690391+ShovelDev@users.noreply.github.com>
2021-11-14 10:49:24 -0300

    Delete pages directory

--- pages/gleckcJS/pkg/gtools.js
+++
@@ -1,7 +0,0 @@
-const $G = function (element){
-	var self = document.querySelector(element)
-
-	function text(text){
-		self.innerText = text
-	}
-}
--- pages/webengine/readme.txt
+++
@@ -1,2 +0,0 @@
-Hi, thanks for downloading WebEngine :D, hope you like it.
-see her tutorials in the file called help.txt
